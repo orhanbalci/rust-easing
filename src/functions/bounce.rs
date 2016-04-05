e0ff1590abcd120d6f9f18005de7f4a446f7a3a6
orhanbalci <=>
2016-04-05 22:45:12 +0300

    bounce ease in and out implemented

--- src/functions/bounce.rs
+++ src/functions/bounce.rs
@@ -1,18 +1,22 @@
 use super::ease::easing;
 struct Bounce;
 
-impl easing for Bounce{
-	fn ease_in(t: f32, b: f32, c: f32, d: f32)->f32{
-	0.0
-	}
+impl easing for Bounce {
+    fn ease_in(t: f32, b: f32, c: f32, d: f32) -> f32 {
+        c - Bounce::ease_out(d-t, 0.0, c, d)
+    }
 
-	fn ease_out(t: f32, b:f32, c:f32, d:f32)->f32{
-	0.0
-	}
+    fn ease_out(t: f32, b: f32, c: f32, d: f32) -> f32 {
+        0.0
+    }
 
 
-	fn ease_in_out(t: f32, b:f32, c:f32, d:f32)->f32{
-	0.0
-	}
+    fn ease_in_out(t: f32, b: f32, c: f32, d: f32) -> f32 {
+        if t < (d / 2.0) {
+            Bounce::ease_in(t * 2.0, 0.0, c, d) * 0.5 + b
+        } else {
+            Bounce::ease_out(t * 2.0 - d, 0.0, c, d) * 0.5 + c * 0.5 + b
+        }
 
+    }
 }
