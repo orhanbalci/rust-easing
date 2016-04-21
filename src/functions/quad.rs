d1565d74e84eda3bcfcad9d83e26122eed5de23c
orhanbalci <orhanbalci@gmail.com>
2016-04-21 11:55:47 +0300

    quad function tests added

--- src/functions/quad.rs
+++ src/functions/quad.rs
@@ -14,11 +14,30 @@ impl Easing for Quad {
     }
 
     fn ease_in_out(t: f32, b: f32, c: f32, d: f32) -> f32 {
-        let mut inner_t = t / d / 2.0;
+        let mut inner_t = t / (d / 2.0);
         if inner_t < 1.0 {
             return (c / 2.0 * (inner_t.powi(2))) + b;
         }
         let temp = inner_t - 1.0;
         return -c / 2.0 * (((inner_t - 2.0) * (temp)) - 1.0) + b;
     }
+}
+
+mod test {
+    use functions::ease::Easing;
+    #[test]
+    fn ease_in() {
+        assert_relative_eq!(super::Quad::ease_in(1.0, 2.0, 3.0, 4.0), 2.187500);
+    }
+
+    #[test]
+    fn ease_out() {
+        assert_relative_eq!(super::Quad::ease_out(1.0, 2.0, 3.0, 4.0), 3.312500);
+    }
+
+    #[test]
+    fn ease_in_out() {
+        assert_relative_eq!(super::Quad::ease_in_out(1.0, 2.0, 3.0, 4.0), 2.37500);
+    }
+
 }
