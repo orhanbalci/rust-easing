869a58745fda35dc8b51710c774d2b7d3ed0db23
orhanbalci <orhanbalci@gmail.com>
2016-04-21 10:02:47 +0300

    expo function tests added

--- src/functions/expo.rs
+++ src/functions/expo.rs
@@ -19,17 +19,36 @@ impl Easing for Expo {
     }
 
     fn ease_in_out(t: f32, b: f32, c: f32, d: f32) -> f32 {
-        if t == d {
+        if t == 0.0 {
             return b;
         }
         if t == d {
             return b + c;
         }
-        let mut inner_t = t / d / 2.0;
+        let mut inner_t = t / (d / 2.0);
         if inner_t < 1.0 {
-            return c / 2.0 * 2_f32.powf(10.0 * (t - 1.0)) + b;
+            return c / 2.0 * 2_f32.powf(10.0 * (inner_t - 1.0)) + b;
         }
         inner_t -= 1.0;
         return c / 2.0 * (-(2_f32.powf(-10.0 * inner_t)) + 2.0) + b;
     }
+}
+
+mod test {
+    use functions::ease::Easing;
+    #[test]
+    fn ease_in() {
+        assert_relative_eq!(super::Expo::ease_in(1.0, 2.0, 3.0, 4.0), 2.016573);
+    }
+
+    #[test]
+    fn ease_out() {
+        assert_relative_eq!(super::Expo::ease_out(1.0, 2.0, 3.0, 4.0), 4.469670);
+    }
+
+    #[test]
+    fn ease_in_out() {
+        assert_relative_eq!(super::Expo::ease_in_out(1.0, 2.0, 3.0, 4.0), 2.046875);
+    }
+
 }
