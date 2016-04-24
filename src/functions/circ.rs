bcef53e99c0bfe3b9506f24286fc5d9cc02c625c
orhanbalci <=>
2016-04-24 19:36:07 +0300

    circ function test added

--- src/functions/circ.rs
+++ src/functions/circ.rs
@@ -39,5 +39,6 @@ mod test {
     #[test]
     fn ease_in_out() {
         assert_relative_eq!(super::Circ::ease_in_out(1.0, 2.0, 3.0, 4.0), 2.200962);
+        assert_relative_eq!(super::Circ::ease_in_out(51.0, 1.0, 100.0, 100.0), 60.949871);
     }
 }
