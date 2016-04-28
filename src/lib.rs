f7c3a40261d97bd62e9ed32a76afc266aa6e5c97
orhanbalci <orhanbalci@gmail.com>
2016-04-28 21:47:04 +0300

    example doc added

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,19 @@
 //! This library implements Robert Penner's easing functions
+//! Easing functions calculate rate of change of a property over time
+//!
+//! #example
+//!
+//! let mut x: [f32; 100] = [0.0; 100];
+//! let mut y: [f32; 100] = [0.0; 100];
+//! for i in 0..100 {
+//!     x[i] = i as f32;
+//!     y[i] = i as f32;
+//! }
+//! println!("Before {:?}", &y[..]);
+//! y.iter_mut().map(|a| *a = Back::ease_in(*a, 0f32, 100f32, 100f32)).count();
+//! println!("After {:?}", &y[..]);
+//!
+//!
 #[macro_use]
 extern crate approx;
 
