079d5febbb964ce9263ac77ca44b737214d02250
Kerollmops <renault.cle@gmail.com>
2016-12-08 00:17:34 +0100

    Clear the Bounce easing functions

--- src/functions/bounce.rs
+++ src/functions/bounce.rs
@@ -1,4 +1,5 @@
 use super::ease::Easing;
+
 /// This struct captures Bounce easing functions
 pub struct Bounce;
 
@@ -7,22 +8,21 @@ impl Easing for Bounce {
         c - Bounce::ease_out(d - t, 0.0, c, d) + b
     }
 
-    fn ease_out(t: f32, b: f32, c: f32, d: f32) -> f32 {
-        let mut inner_t = t / d;
-        if inner_t < 1.0 / 2.75 {
-            c * (7.5625 * inner_t * inner_t) + b
-        } else if inner_t < 2.0 / 2.75 {
-            inner_t -= 1.5 / 2.75;
-            c * (7.5625 * inner_t * inner_t + 0.75) + b
-        } else if inner_t < 2.5 / 2.75 {
-            inner_t -= 2.25 / 2.75;
-            c * (7.5625 * inner_t * inner_t + 0.9375) + b
+    fn ease_out(mut t: f32, b: f32, c: f32, d: f32) -> f32 {
+        t = t / d;
+        if t < 1.0 / 2.75 {
+            c * (7.5625 * t * t) + b
+        } else if t < 2.0 / 2.75 {
+            t -= 1.5 / 2.75;
+            c * (7.5625 * t * t + 0.75) + b
+        } else if t < 2.5 / 2.75 {
+            t -= 2.25 / 2.75;
+            c * (7.5625 * t * t + 0.9375) + b
         } else {
-            inner_t -= 2.625 / 2.75;
-            c * (7.5625 * inner_t * inner_t + 0.984375) + b
+            t -= 2.625 / 2.75;
+            c * (7.5625 * t * t + 0.984375) + b
         }
     }
-
 
     fn ease_in_out(t: f32, b: f32, c: f32, d: f32) -> f32 {
         if t < (d / 2.0) {
