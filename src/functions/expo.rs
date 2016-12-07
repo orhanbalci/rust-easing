39ae426f7f78f2fc781e96b899e1444b6508994e
Kerollmops <renault.cle@gmail.com>
2016-12-08 00:27:51 +0100

    Clear the Expo easing functions

--- src/functions/expo.rs
+++ src/functions/expo.rs
@@ -1,4 +1,5 @@
 use super::ease::Easing;
+
 /// This struct captures Expo easing functions
 pub struct Expo;
 
@@ -19,19 +20,21 @@ impl Easing for Expo {
         }
     }
 
-    fn ease_in_out(t: f32, b: f32, c: f32, d: f32) -> f32 {
+    fn ease_in_out(mut t: f32, b: f32, c: f32, d: f32) -> f32 {
         if t == 0.0 {
             return b;
         }
         if t == d {
             return b + c;
         }
-        let mut inner_t = t / (d / 2.0);
-        if inner_t < 1.0 {
-            return c / 2.0 * 2_f32.powf(10.0 * (inner_t - 1.0)) + b;
+        t = t / (d / 2.0);
+        if t < 1.0 {
+            c / 2.0 * 2_f32.powf(10.0 * (t - 1.0)) + b
         }
-        inner_t -= 1.0;
-        return c / 2.0 * (-(2_f32.powf(-10.0 * inner_t)) + 2.0) + b;
+        else {
+            t -= 1.0;
+            c / 2.0 * (-(2_f32.powf(-10.0 * t)) + 2.0) + b
+        }
     }
 }
 
