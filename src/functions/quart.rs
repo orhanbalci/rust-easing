db1cbaea359cac04e007a3d5aef25b0e50441553
Kerollmops <renault.cle@gmail.com>
2016-12-08 00:30:10 +0100

    Clear the Quart easing functions

--- src/functions/quart.rs
+++ src/functions/quart.rs
@@ -1,27 +1,29 @@
 use super::ease::Easing;
+
 /// This struct captures Quart easing functions
 pub struct Quart;
 
 impl Easing for Quart {
-    fn ease_in(t: f32, b: f32, c: f32, d: f32) -> f32 {
-        let inner_t = t / d;
-        c * inner_t.powi(4) + b
+    fn ease_in(mut t: f32, b: f32, c: f32, d: f32) -> f32 {
+        t = t / d;
+        c * (t * t * t * t) + b
     }
 
-    fn ease_out(t: f32, b: f32, c: f32, d: f32) -> f32 {
-        let inner_t = t / d - 1.0;
-        -c * (inner_t.powi(4) - 1.0) + b
+    fn ease_out(mut t: f32, b: f32, c: f32, d: f32) -> f32 {
+        t = t / d - 1.0;
+        -c * ((t * t * t * t) - 1.0) + b
     }
 
-    fn ease_in_out(t: f32, b: f32, c: f32, d: f32) -> f32 {
-        let mut inner_t = t / (d / 2.0);
-
-        if inner_t < 1.0 {
-            return c / 2.0 * inner_t * inner_t * inner_t * inner_t + b;
+    fn ease_in_out(mut t: f32, b: f32, c: f32, d: f32) -> f32 {
+        t = t / (d / 2.0);
+        if t < 1.0 {
+            c / 2.0 * (t * t * t * t) + b
+        }
+        else {
+            t -= 2.0;
+            -c / 2.0 * ((t * t * t * t) - 2.0) + b
         }
 
-        inner_t -= 2.0;
-        return -c / 2.0 * (inner_t.powi(4) - 2.0) + b;
     }
 }
 
