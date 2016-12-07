3525a228f8bcad18114cf199eb3f3e80a707355d
Kerollmops <renault.cle@gmail.com>
2016-12-08 00:21:23 +0100

    Clear the Cubic easing functions

--- src/functions/cubic.rs
+++ src/functions/cubic.rs
@@ -4,23 +4,25 @@ use super::ease::Easing;
 pub struct Cubic;
 
 impl Easing for Cubic {
-    fn ease_in(t: f32, b: f32, c: f32, d: f32) -> f32 {
-        let inner_t = t / d;
-        c * inner_t.powi(3) + b
+    fn ease_in(mut t: f32, b: f32, c: f32, d: f32) -> f32 {
+        t = t / d;
+        c * (t * t * t) + b
     }
 
-    fn ease_out(t: f32, b: f32, c: f32, d: f32) -> f32 {
-        let inner_t = t / d - 1.0;
-        c * (inner_t.powi(3) + 1.0) + b
+    fn ease_out(mut t: f32, b: f32, c: f32, d: f32) -> f32 {
+        t = t / d - 1.0;
+        c * ((t * t * t) + 1.0) + b
     }
 
-    fn ease_in_out(t: f32, b: f32, c: f32, d: f32) -> f32 {
-        let mut inner_t = t / (d / 2.0);
-        if inner_t < 1.0 {
-            return c / 2.0 * inner_t.powi(3) + b;
+    fn ease_in_out(mut t: f32, b: f32, c: f32, d: f32) -> f32 {
+        t = t / (d / 2.0);
+        if t < 1.0 {
+            c / 2.0 * (t * t * t) + b
         }
-        inner_t = inner_t - 2.0;
-        return c / 2.0 * (inner_t.powi(3) + 2.0) + b;
+        else {
+            t = t - 2.0;
+            c / 2.0 * (t * t * t + 2.0) + b
+        }
     }
 }
 
