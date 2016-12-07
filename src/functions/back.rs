cc17a23be213ff8d0795a2a9dc91fde86010b927
Kerollmops <renault.cle@gmail.com>
2016-12-08 00:15:27 +0100

    Clear the Back easing functions

--- src/functions/back.rs
+++ src/functions/back.rs
@@ -1,32 +1,33 @@
 use super::ease::Easing;
+
 /// This struct captures Back easing functions
 pub struct Back;
 
 impl Easing for Back {
-    fn ease_in(t: f32, b: f32, c: f32, d: f32) -> f32 {
+    fn ease_in(mut t: f32, b: f32, c: f32, d: f32) -> f32 {
         let s = 1.70158_f32;
-        let inner_t = t / d;
-        let post_fix = inner_t;
-        c * (post_fix) * inner_t * ((s + 1.0) * inner_t - s) + b
+        t = t / d;
+        c * t * t * ((s + 1.0) * t - s) + b
     }
 
-    fn ease_out(t: f32, b: f32, c: f32, d: f32) -> f32 {
+    fn ease_out(mut t: f32, b: f32, c: f32, d: f32) -> f32 {
         let s = 1.70158_f32;
-        let inner_t = (t / d) - 1.0;
-        c * (inner_t * inner_t * ((s + 1.0) * inner_t + s) + 1.0) + b
+        t = (t / d) - 1.0;
+        c * (t * t * ((s + 1.0) * t + s) + 1.0) + b
     }
 
-    fn ease_in_out(t: f32, b: f32, c: f32, d: f32) -> f32 {
+    fn ease_in_out(mut t: f32, b: f32, c: f32, d: f32) -> f32 {
         let mut s = 1.70158_f32;
-        let mut inner_t = t / (d / 2.0);
-        if inner_t < 1.0 {
+        t = t / (d / 2.0);
+        if t < 1.0 {
             s *= 1.525f32;
-            return c / 2.0 * (inner_t * inner_t * ((s + 1.0) * inner_t - s)) + b;
+            c / 2.0 * (t * t * ((s + 1.0) * t - s)) + b
         }
-        inner_t -= 2.0;
-        let post_fix: f32 = inner_t;
-        let inner_s = s * 1.525f32;
-        return c / 2.0 * (post_fix * inner_t * ((inner_s + 1.0) * inner_t + inner_s) + 2.0) + b;
+        else {
+            t -= 2.0;
+            s = s * 1.525f32;
+            c / 2.0 * (t * t * ((s + 1.0) * t + s) + 2.0) + b
+        }
     }
 }
 
